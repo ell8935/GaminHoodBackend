2fdb84395fb6b0b0629cbe6f0230840404188c66
Elad <ell8935@gmail.com>
2022-12-15 20:09:27 +0200

    Fixx

--- src/func/getDataEpicGames.js
+++ src/func/getDataEpicGames.js
@@ -1,9 +1,9 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 
 const getDataEpicGames = async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    // executablePath: "./node_modules/chromium/lib/chromium/chrome-win/chrome",
+    executablePath: "./node_modules/chromium/lib/chromium/chrome-win/chrome",
     args: ["--no-sandbox"],
   });
 
