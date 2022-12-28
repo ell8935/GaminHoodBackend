a679e02cfdaa4e584d87506f46b548b933a04cd1
Elad <ell8935@gmail.com>
2022-12-28 15:30:09 +0200

    Interval every 10min

--- src/func/getAllDataInterval.js
+++ src/func/getAllDataInterval.js
@@ -2,7 +2,7 @@ import getAllData from "./getAllData.js";
 
 const getAllDataInterval = () => {
   getAllData();
-  const minutes = 20 * 60 * 1000;
+  const minutes = 10 * 60 * 1000;
   setInterval(() => {
     getAllDataInterval();
   }, minutes);
