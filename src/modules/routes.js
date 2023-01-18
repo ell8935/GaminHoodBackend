13f48160cfbad976104b509d4918b1b90df5101f
Elad <ell8935@gmail.com>
2023-01-18 22:40:47 +0200

    Fix

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -3,7 +3,7 @@ import { getData, sendEmail } from "./controllers/index.js";
 
 const connectionRoute = express.Router();
 
-connectionRoute.get("/getData", getData);
+connectionRoute.get("/", getData);
 connectionRoute.post("/sendEmail", sendEmail);
 
 export { connectionRoute };
