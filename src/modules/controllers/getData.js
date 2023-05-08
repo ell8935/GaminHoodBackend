549e2aa1162e5b205bc082f2a36d1210883357d1
Elad <ell8935@gmail.com>
2023-05-08 19:47:17 +0300

    Database detour

--- src/modules/controllers/getData.js
+++ src/modules/controllers/getData.js
@@ -1,14 +1,19 @@
 import { connectToDB } from "../../../database/func/index.js";
+import getAllData from "../../shared/utils/getAllData.js";
 
 const getData = async (_req, res, next) => {
   try {
-    let sql = `SELECT * FROM game `;
-    connectToDB.all(sql, (err, rows) => {
-      if (err) console.log(err);
-      console.log(rows);
+    // let sql = `SELECT * FROM game `;
+    // connectToDB.all(sql, (err, rows) => {
+    //   if (err) console.log(err);
+    //   console.log(rows);
 
-      return res.status(200).json(rows);
-    });
+    //   return res.status(200).json(rows);
+    // });
+
+    const data = await getAllData();
+
+    return res.status(200).json(data); // Fix till migrating to a hosted Database
   } catch (err) {
     return next(err);
   }
